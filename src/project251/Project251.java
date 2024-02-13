4c9577ebf61a1ec70cb148671133df4638aa7a36
mahaba <mahaba.008@gmail.com>
2024-02-13 19:20:14 +0300

    Group repo

--- src/project251/Project251.java
+++ src/project251/Project251.java
@@ -5,8 +5,9 @@ public class Project251 {
 
    
     public static void main(String[] args) {
- System.out.print("Hi");
- System.out.print("Hey");
+        System.out.print("Hi");
+        System.out.print("Hey");
+        System.out.println("Hello I'm mahaba");
 
     }
     

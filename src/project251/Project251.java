d050c5d3ef47897dfc394936f9ea4df2476d97aa
LenaAbdulrazak <lrazak@stu.kau.edu.sa>
2024-04-07 06:05:18 +0300

    HH

--- src/project251/Project251.java
+++ src/project251/Project251.java
@@ -8,16 +8,65 @@ import java.util.Scanner;
     
 
 public class Project251 extends Menu{
-          
+    
+    public static void menu() {
+        System.out.println("\n\n******************");
+        System.out.println("****** Restaurant Menu *******");
+        System.out.println("******************");
+        System.out.println("1: View Menu Items");
+        System.out.println("2: Order");
+        System.out.println("3: Reserve Table");
+        System.out.println("4: Make Payment");
+        System.out.println("5: Exit");
+        System.out.println(" ---------------------------------------------------");
+        System.out.print("\nChoose from the menu: ");
+    }
+    
     
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int selectedOption;
         
         
       System.out.println("Welcome to the Restaurant!");
        Menu m = new Menu();
-        m.DisplayMenu();
+       do {
+            menu();
+            selectedOption = input.nextInt();
 
+            switch (selectedOption) {
+                case 1: {
+                    m.DisplayMenu();
+                    break;
+                }
+                case 2: {
+                    System.out.print("Enter the item name to order: ");
+                    String itemName = input.next();
+                    //MenuItem itemToOrder = m.findItemByName(itemName);
+                    //if (itemToOrder != null) {
+                       // System.out.println("Ordered: " + itemToOrder.getName() + " - $" + itemToOrder.getPrice());
+                    //} else {
+                    //    System.out.println("Item not found in the menu!");
+                   // }
+                    break;
+                }
+                case 3: {
+                    System.out.println("Table reserved!");
+                    break;
+                }
+                case 4: {
+                    System.out.println("Payment made!");
+                    break;
+                }
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+                    break;
+            }
+        } while (selectedOption != 5);
+        
+        input.close();
     }
-
 }

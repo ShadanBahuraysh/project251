e1b1a9e4d261f92b089ffa02d28a4aec4a180fba
mahaba <mahaba.008@gmail.com>
2024-04-23 16:32:44 +0300

    Group repo

--- src/project251/Order.java
+++ src/project251/Order.java
@@ -1,4 +1,4 @@
-package project251;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -20,22 +20,26 @@ private List<MenuItem> itemsOrdered;
             displayMenuItem(item);
         }
     }
+    public List<MenuItem> getOrderedItems() {
+    return itemsOrdered;
+}
     
     private void displayMenuItem(MenuItem item) {
         System.out.println("- " + item.toString());
     }
     
-    public static void handleOrder(Scanner input, Menu menu, Order order) {
+    public static void makeOrder(Scanner input, Menu menu, Order order) {
         boolean condition = false;
         String itemName = "";
         while (!condition) {
             System.out.print("Enter the item name to order: ");
             itemName = input.next();
-            MenuItem orderedItem = menu.findItemByName(itemName);
+            MenuItem orderedItem = menu.findMenuItemsByName(itemName);
             
             if (orderedItem != null) {
                 order.addItem(orderedItem);
                 System.out.println("Ordered: " + orderedItem.getName() + " - $" + orderedItem.getPrice());
+                
                 System.out.print("Is that all ?");
                 String yesNo = input.next().toLowerCase();
                 if (yesNo.equals("yes")) {
